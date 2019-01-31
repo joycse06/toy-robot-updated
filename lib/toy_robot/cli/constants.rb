d043e9dffb7841b067b2fd79a71d906a5137ef87
Prasenjit Nag <joyruet06@gmail.com>
2019-02-01 07:07:31 +1100

    Update REPL banner

--- lib/toy_robot/cli/constants.rb
+++ lib/toy_robot/cli/constants.rb
@@ -13,7 +13,7 @@ module ToyRobot
         | This program let the user simulate a robot's movement on top of a |
         | 5X5 Table (grid).                                                 |
         | Valid commands are:                                               |
-        | PLACE x,y,WEST                                                    |
+        | PLACE X,Y,F                                                       |
         | MOVE                                                              |
         | LEFT                                                              |
         | RIGHT                                                             |
