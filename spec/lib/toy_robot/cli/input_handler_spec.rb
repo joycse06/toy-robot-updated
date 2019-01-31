0033f70767458515cf669196ad998c9e20ced66b
Prasenjit Nag <joyruet06@gmail.com>
2019-02-01 06:06:41 +1100

    Add more integration specs to cover most (if not all) error conditions

--- spec/lib/toy_robot/cli/input_handler_spec.rb
+++ spec/lib/toy_robot/cli/input_handler_spec.rb
@@ -94,6 +94,71 @@ module ToyRobot
             end
           end
 
+          context 'when empty command is entered' do
+            it 'displays error message' do
+              expect { input_handler.handle_input('') }
+                .to output("Please enter a valid command.\n").to_stdout
+            end
+          end
+
+          context 'when arguments are entered for any command other than PLACE' do
+            before do
+              input_handler.handle_input('PLACE 4,4,WEST')
+            end
+
+            it 'displays error message' do
+              expect { input_handler.handle_input('MOVE THERE') }
+                .to output("Command: 'MOVE' does not take argument(s).\n").to_stdout
+            end
+
+            context 'when error message has been displayed' do
+              before do
+                input_handler.handle_input('MOVE THERE')
+              end
+
+              it 'can still accept further valid arguments' do
+                expect { input_handler.handle_input('MOVE'); input_handler.handle_input('REPORT') }
+                  .to output(/3,4,WEST/).to_stdout
+              end
+            end
+          end
+
+          context 'when PLACE command is entered again' do
+            context 'when place command arguments are  valid' do
+              before do
+                input_handler.handle_input('PLACE 4,4,WEST')
+              end
+
+              it 'reports correct position' do
+                expect { input_handler.handle_input('REPORT') }
+                  .to output(/4,4,WEST/).to_stdout
+              end
+            end
+
+            context 'when one of place command arguments is invalid' do
+              context 'when one of the co-ordinates is out of range' do
+                it 'displays error' do
+                  expect { input_handler.handle_input('PLACE 5,2,EAST') }
+                    .to output("Please enter valid co-ordinates. Valid Range of co-ordinate values are: For X: '0-4' For Y: '0-4'\n").to_stdout
+                end
+              end
+
+              context 'when one of the co-ordinates is not coercible integer' do
+                it 'displays error' do
+                  expect { input_handler.handle_input('PLACE X,2,EAST') }
+                    .to output("Please enter valid co-ordinates. Only integers are valid.\n").to_stdout
+                end
+              end
+
+              context 'when the entered direction (or face) is invalid' do
+                it 'displays error' do
+                  expect { input_handler.handle_input('PLACE 3,2,UNKNOWN') }
+                    .to output("Please enter a valid direction. Entered: 'UNKNOWN'. Valid values are: NORTH,EAST,SOUTH,WEST\n").to_stdout
+                end
+              end
+            end
+          end
+
           context 'when at the edge' do
             before do
               input_handler.handle_input('MOVE')
@@ -105,6 +170,23 @@ module ToyRobot
             it 'displays error message if asked to move further' do
               expect { input_handler.handle_input('MOVE') }
                 .to output("Moving NORTH from (0,4) will push robot out of table. Ignoring.\n").to_stdout
+            end
+
+            context 'when error messages has been displayed' do
+              before do
+                input_handler.handle_input('MOVE') # shows the error message
+                input_handler.handle_input('RIGHT') # to ensure normal commands are still accepted
+              end
+
+              it 'reports the position correctly' do
+                expect { input_handler.handle_input('REPORT') }
+                  .to output(/0,4,EAST/).to_stdout
+              end
+
+              it 'can move into other directions correctly' do
+                expect { input_handler.handle_input('MOVE'); input_handler.handle_input('REPORT') }
+                  .to output(/1,4,EAST/).to_stdout
+              end
             end
           end
         end
